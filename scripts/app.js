2359219f9a635034426802870997f477e6fd07e0
Alex <ahawknj15@gmail.com>
2019-09-13 21:03:15 -0400

    see work button smoothly scrolls to projects section

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,21 @@
 $(() => {
 
   // event handlers
+  //scroll between slides -- code help from attacomsian.com
   $('#main-nav').on('click', 'a[href^="#"]', function(event) {
     var targetHref = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(targetHref).offset().top
     }, 250);
+    event.preventDefault();
+  });
+
+  //see my work button
+  $('.slide-one-wrapper').on('click', 'a[href^="#"]', function(event) {
+    var targetHref = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(targetHref).offset().top
+    }, 300);
     event.preventDefault();
   });
 
